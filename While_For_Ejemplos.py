e05149f591035a44221170735f0e08e3f11dbb76
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 13:18:32 -0600

    Ejemplo 4. Ejemplos prácticos while

---
+++ While_For_Ejemplos.py
@@ -0,0 +1,47 @@
+print("---------------------------")
+#num = 20
+#while num >=1:
+ #   if num % 3 == 0:
+  #      print(num)
+   #     break
+   # num +=1
+print("---------------------------")
+#num = 1
+#while num !=10:
+  #  print(num)
+  #  num +=2
+  #  break
+print("----------------------------")
+#ciclo for leer letra por letra 
+#palabra = "marvin"
+#for letra in palabra:
+    #print(letra,end='')
+
+#Se detiene con el break hasta m-a-r
+#for letra in palabra:
+    #if letra =='v':
+      #break
+    #print(letra,end='-')
+
+# Cuenta con el for y con un rango de 0,1,2,3,4,5,6,7,8,9
+#for i in range(0,10):
+   #print(i,end='')
+
+# Conocer la Tabla de multiplicar utilizando for y range.
+#print("-----------------------")
+#print('Tabla de multiplicar')
+#tabla = 0
+#num = 0
+
+#table =int(input("M: ")) 
+
+#for j in range(1,11):
+#  result = table * j
+#  print(f'{table}*{j} = {table*j}')
+
+
+
+
+#num = int(input("Ingrese el número: "))
+#for i in range(0,10):
+#  print(num, " * ", i , " = ", num * i)
