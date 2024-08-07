2fed76095886d300556df75002ee8972ec56a044
MarvinTrey5 <santosmarvin201@gmail.com>
2024-08-07 13:21:49 -0600

    Ejercicio 3. While Suma Rango De Impares

---
+++ While_SumaImpares_Rango.py
@@ -0,0 +1,17 @@
+# Hacer un ejercicio en Python
+# que mediante un ciclo while sume 
+# Únicamente los números impares del rango 1 al 20.
+
+print("-----------------------")
+print("Se presentan los números impares del rango")
+suma=0
+i=1
+while i<=20:
+      print(i," ",end=" ")
+      suma=suma+i
+      i=i+2
+print("")
+print("------------------------")
+print("El total de la suma de los números impares es: ",suma)
+
+print("Fin del ejercicio")
